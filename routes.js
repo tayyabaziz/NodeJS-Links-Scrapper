daeb1051134e04cb783b4567087725de01921f3e
tayyabaziz <tayyabaziz5@gmail.com>
2019-12-11 13:12:27 +0500

    Update

--- routes.js
+++ routes.js
@@ -1,46 +1,8 @@
 const express = require('express')
-const axios = require('axios')
-const cheerio = require('cheerio')
-const scrap_urls = require('./scrap_urls')
-const scrap_images = require('./scrap_images')
 const router = express.Router()
+const controller = require('./controller')
 
-router.get('/scrap_urls', async (req, res) => {
-  const urlLink = req.query.url
-  try {
-    if (urlLink != "" && urlLink != null) {
-      const data = await scrap_urls(axios, cheerio, urlLink)
-      if (data.length > 0)
-        res.status(200).json({ status: 200, message: data })
-      else
-        res.status(404).json({ status: 404, message: "No data found" })
-    }
-    else {
-      res.status(500).json({ status: 500, message: "Error Occured" })
-    }
-  } catch (error) {
-    res.status(404).json({ status: 404, message: "No data found" })
-  }
-  res.end()
-})
-
-router.get('/scrap_images', async (req, res) => {
-  const urlLink = req.query.url
-  try {
-    if (urlLink != "" && urlLink != null) {
-      const data = await scrap_images(axios, cheerio, urlLink)
-      if (data.length > 0)
-        res.status(200).json({ status: 200, message: data })
-      else
-        res.status(404).json({ status: 404, message: "No data found" })
-    }
-    else {
-      res.status(500).json({ status: 500, message: "Error Occured" })
-    }
-  } catch (error) {
-    res.status(404).json({ status: 404, message: "No data found" })
-  }
-  res.end()
-});
+router.get('/scrap_urls', controller.scrapUrls)
+router.get('/scrap_images', controller.scrapImages);
 
 module.exports = router;
